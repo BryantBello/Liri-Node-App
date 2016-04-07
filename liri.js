0521664028da72be1a410b2e04286bef6b6bbe2f
bryantbello <bryant.bello@outlook.com>
2016-04-06 23:05:54 -0400

    packageJSON

--- liri.js
+++ liri.js
@@ -8,3 +8,23 @@ var params = process.argv.slice(2); //USE TO IGNORE FIRST TWO ARGV TO LIMIT MIST
 
 
 
+function myTweets(){
+   
+	//Obtained from the Twitter API DOCS PAGE
+   var client = new Twitter({  
+      consumer_key: keys.twitterKeys.consumer_key,
+      consumer_secret: keys.twitterKeys.consumer_secret,
+      access_token_key: keys.twitterKeys.access_token_key,
+      access_token_secret: keys.twitterKeys.access_token_secret
+    });
+    
+  client.get('statuses/user_timeline', {screen_name: 'bryant_bello', count: 20 }, function (error, data, response){
+    if(error) throw error;
+    for(var i = 0; i < data.length; i++){
+
+		    var tweetResults = data[i].text + "\n";
+		    console.log(tweetResults); 
+      
+      };
+  });
+}  
