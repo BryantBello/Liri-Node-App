8a7e63b32b61e4e9486de5ae6a751e02490df8d7
bryantbello <bryant.bello@outlook.com>
2016-06-26 18:37:44 -0400

    comments added

--- omdb.js
+++ omdb.js
@@ -1,3 +1,5 @@
+//stand alone OMDB
+
 var request = require('request');
 request('http://www.omdbapi.com/?t=The+Notebook&y=&plot=short&r=json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
